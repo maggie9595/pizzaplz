e00878c23b0e3eed39e50e93182b0cc58b8afe3d
Chris Wei <cwei1994@gmail.com>
2014-09-13 00:59:09 -0400

    moved js to sep file

---
+++ script.js
@@ -0,0 +1,39 @@
+var lng;
+var lat;
+
+function GetClock(){
+  var d = new Date();
+  var nhour  = d.getHours();
+  var nmin   = d.getMinutes();
+       if(nhour ==  0) {ap = " AM";nhour = 12;} 
+  else if(nhour <= 11) {ap = " AM";} 
+  else if(nhour == 12) {ap = " PM";} 
+  else if(nhour >= 13) {ap = " PM";nhour -= 12;}
+
+  if(nmin <= 9) {nmin = "0" +nmin;}
+
+  document.getElementById('clockbox').innerHTML="It's "+nhour+":"+nmin+ap+".";
+  setTimeout("GetClock()", 1000);
+  }
+
+function storeLocation(location)
+{
+  lat = position.coords.latitude;
+  lng = position.coords.longitude;
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(storeLocation);
+    } else { 
+        alert("Can't find you!"); //implement other methods of getting loc
+    }
+}
+
+
+
+
+
+
+
+window.onload=GetClock;
