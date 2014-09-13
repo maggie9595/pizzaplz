d9772d0d5247afbb2649be531f48bda728537dc8
Adit Namdev <aditn4@gmail.com>
2014-09-12 23:24:05 -0400

    testing location services

--- locationservices.php
+++ locationservices.php
@@ -1,15 +1,5 @@
 <?
-//$html = file_get_contents('http://www.where-am-i.net/');
-$c = curl_init('http://www.where-am-i.net/');
-curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-$html = curl_exec($c);
-
-if (curl_error($c))
-    die(curl_error($c));
-
-// Get the status code
-$status = curl_getinfo($c, CURLINFO_HTTP_CODE);
-
-curl_close($c);
+$html = file_get_contents('http://www.where-am-i.net/');
+echo "$html"
 
 ?>
